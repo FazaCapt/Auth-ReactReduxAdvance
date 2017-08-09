0cd5121807c6baed4ee92c4940c665637ec26445
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-09 13:03:42 +0700

    Searching for Users

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,19 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-    res.send({ success: 'true'});
+    // console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // see if a user with the given email exist
+    User.findOne({ email: email }, function(err, existingUser) {
+
+    });
+
+    // if a user with email does exist, return an error
+
+    // if a user with email does nNOT exist, create and save user record
+
+
+    // respond to request indicating the user was created
 }
